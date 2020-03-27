9e72ece39a5e36404982c7d802351cb62359a003
Renatto NL <renattonl@gmail.com>
2020-03-26 21:59:06 -0500

    modificando el post single

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -6,18 +6,25 @@
         <div class="col-md-8">
           <div class="card mb-4">
             <div class="card-body">
-                <h5 class="card-title">
-                  {{$post->title}}
-                </h5>
-                <p class="card-text">
-                  {{$post->body}}
-                </p>
-                <div class="text-muted mb-0">
-                  <em>
-                    &ndash; {{$post->user->name}}
-                  </em>
-                  {{ $post->created_at->format("d M Y") }}
+              @if ($post->image)
+                <img src="{{$post->get_image}}" alt="{{$post->title}}" class="card-img-top" />
+              @elseif ($post->iframe)
+                <div class="embed-responsive embed-responsive-16by9">
+                  {!! $post->iframe !!}
                 </div>
+              @endif
+              <h5 class="card-title">
+                {{$post->title}}
+              </h5>
+              <p class="card-text">
+                {{$post->body}}
+              </p>
+              <div class="text-muted mb-0">
+                <em>
+                  &ndash; {{$post->user->name}}
+                </em>
+                {{ $post->created_at->format("d M Y") }}
+              </div>
             </div>
           </div>
         </div>
